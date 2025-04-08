76007b5b7e615afa744c1019cf1a2eab91de9412
JuliaSoltys <juliasoltys@op.pl>
2025-04-08 19:01:00 +0200

    chore: mvt pattern

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-users = [
-    {"name": "Julia", "location": "Rzeszów", "posts": 500},
-    {"name": "Michał", "location": "Krasnystaw", "posts": 400},
-    {"name": "Ksavier", "location": "Grudziądz", "posts": 300},
-    {"name": "Damian", "location": "Krakow", "posts": 200},
+from utils.controller import get_user_info
+from utils.model import users
 
-]
+def main():
+    get_user_info(users)
 
 
-def get_user_info(users_data: list)->None:
-    for user in users_data:
-       print(f"Twój znajomy {user['name']}, z miejscowości {user['location']}, opublikował {user['posts']} postów")
 
-
-get_user_info(users)
+if __name__ == "__main__":
+    main()
---
+++ utils/model.py
@@ -0,0 +1,7 @@
+users = [
+    {"name": "Julia", "location": "Rzeszów", "posts": 500},
+    {"name": "Michał", "location": "Krasnystaw", "posts": 400},
+    {"name": "Ksavier", "location": "Grudziądz", "posts": 300},
+    {"name": "Damian", "location": "Krakow", "posts": 200},
+
+]
