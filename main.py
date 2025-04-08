a4d2db2bfcfc92a07d4b3122c40416709c4b09a5
JuliaSoltys <juliasoltys@op.pl>
2025-04-08 19:16:33 +0200

    wip: prepared menu

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from utils.controller import get_user_info
 from utils.model import users
 
+
 def main():
-    get_user_info(users)
+    while True:
+            print("========MENU=========")
+            print("0 - zamknij aplikację")
+            print("1 - wyświetl co u znajomych")
+            print("2 -dodaj nowego użytkownika")
+            print("3 - usuń użytkownika")
+            print("4 - edytuj użytkownika")
+            print("========MENU=========")
+
+            choice = input("wybierz opcję menu")
+            if choice == "0":
+                break
+                if choice == "1":
+                    get_user_info(users)
 
 
 
