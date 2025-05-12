9e31171fde797ed4446b3a679d9d58606fe6d374
JuliaSoltys <juliasoltys@op.pl>
2025-05-12 09:30:45 +0200

    wip: gui

--- main.py
+++ main.py
@@ -1,26 +1,76 @@
-from utils.controller import get_user_info, add_user, remove_user, update_user, get_map
-from utils.model import users
+from importlib.metadata import entry_points
+from tkinter import *
 
 
-def main():
-    while True:
-        print("==============MENU================")
-        print('0 - zamknij aplikacje')
-        print('1 - wyswietl co u znajomych')
-        print('2 - dodaj nowego użytkownika')
-        print('3 - usuń użytkownika')
-        print('4 - edytuj uzytkownika')
-        print("5 - Przygotuj mapę znajomych")
-        print("==============MENU================")
-
-        choice = input('wybierz opcje menu')
-        if choice == '0': break
-        if choice == '1': get_user_info(users)
-        if choice == '2': add_user(users)
-        if choice == '3': remove_user(users)
-        if choice == '4': update_user(users)
-        if choice == '5': get_map(users)
 
 
-if __name__ == "__main__":
-    main()
+root = Tk()
+
+root.title("mapbook_js")
+root.geometry("1200x700")
+
+ramka_lista_obiektow = Frame(root)
+ramka_formularz = Frame(root)
+ramka_szczegoly_obiektow = Frame(root)
+
+ramka_lista_obiektow.grid(row=0, column=0, padx=50)
+ramka_formularz.grid(row=0, column=1)
+ramka_szczegoly_obiektow.grid(row=1, column=0)
+
+# ramka_lista_obiektow
+
+label_lista_obiektow = Label(ramka_lista_obiektow, text="Lista znajomych")
+label_lista_obiektow.grid(row=0, column=0)
+
+listbox_lista_obiektow = Listbox(ramka_lista_obiektow)
+listbox_lista_obiektow.grid(row=1, column=0, columnspan=3)
+
+button_pokaz_szczegoly = Button(ramka_lista_obiektow,text = 'Pokaż szczegóły')
+button_pokaz_szczegoly.grid(row=3, column=0)
+
+button_usun_obiekt = Button(ramka_lista_obiektow,text = 'Usuń obiekt')
+button_usun_obiekt.grid(row=3, column=1)
+
+button_edytuj_obiekt = Button(ramka_lista_obiektow,text = 'Edytuj obiekt')
+button_edytuj_obiekt.grid(row=3, column=2)
+
+#ramka_forumlarz
+
+label_formularz = Label(ramka_formularz, text="Formularz")
+label_formularz.grid(row=0, column=1, columnspan=2)
+
+label_imie = Label(ramka_formularz, text="Imię")
+label_imie.grid(row=1, column=0, sticky=W)
+
+label_nazwisko = Label(ramka_formularz, text="Nazwisko")
+label_nazwisko.grid(row=2, column=0, sticky=W)
+
+label_miejscowosc = Label(ramka_formularz, text="Miejscowość")
+label_miejscowosc.grid(row=3, column=0, sticky=W)
+
+label_posty = Label(ramka_formularz, text="Liczba postów")
+label_posty.grid(row=4, column=0, sticky=W)
+entry_imie = Entry(ramka_formularz)
+entry_imie.grid(row=1, column=1)
+entry_nazwisko = Entry(ramka_formularz)
+entry_nazwisko.grid(row=2, column=1)
+entry_miejscowosc = Entry(ramka_formularz)
+entry_miejscowosc.grid(row=3, column=1)
+entry_posty = Entry(ramka_formularz)
+entry_posty.grid(row=4, column=1)
+
+button_dodaj_obiekt = Button(ramka_formularz, text = 'Dodaj')
+button_dodaj_obiekt.grid(row=5, column=0, columnspan=2)
+
+#ramka_szczegoly_obiektu
+
+label_szczegoly_obiektow = Label(ramka_szczegoly_obiektow, text="Szczegóły użytkownika:")
+label_szczegoly_obiektow.grid(row=0, column=0)
+
+label_imie_szczegoly_obiektu = Label(ramka_szczegoly_obiektow, text='Imię: ')
+label_imie_szczegoly_obiektu.grid(row=1, column=0)
+
+
+
+
+root.mainloop()
--- utils/model.py
+++
@@ -1,7 +0,0 @@
-users = [
-    {"name": "Julia", "location": "Rzeszów", "posts": 500},
-    {"name": "Michał", "location": "Krasnystaw", "posts": 400},
-    {"name": "Ksavier", "location": "Grudziądz", "posts": 300},
-    {"name": "Damian", "location": "Kraków", "posts": 200},
-
-]
